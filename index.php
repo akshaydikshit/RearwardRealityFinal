5158fc44199940e0f490ecfd82d1a83a86521f7d
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-13 12:56:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-$response->say('Chapeau!', ['voice' => 'woman', 'language' => 'fr']);
-
+$gather = $response->gather(['input' => 'speech dtmf', 'timeout' => 3,'numDigits' => 1]);
+$gather->say('Please press 1 or say sales for sales.');
 echo $response;
+
 ?>
