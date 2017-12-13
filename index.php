d3ab461a52dd92fcce31478c09550942344ccdb2
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-13 12:49:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,14 @@
-<?php 
- 
-// Get the PHP helper library from 
-require_once 'vendor/autoload.php';use Twilio\Rest\Client; 
- 
-$account_sid = 'ACf008ac9a8d930086e0f663674f37597c'; 
-$auth_token = '1d1204cad6b8a0c6184a87b5f1b02766'; 
-$client = new Client($account_sid, $auth_token); 
- 
+<?php
 
+require_once './vendor/autoload.php';
 
-$client->messages->create(
-    // the number you'd like to send the message to
-    '+19173490168',
-    array(
-        // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+16464551741',
-        // the body of the text message you'd like to send
-        'body' => 'Hello From Akshay Dikshit CS 643 FALL 2017'
-    )
-);
+use Twilio\Twiml;
+
+$response = new Twiml();
+$gather = $response->gather(['input' => 'speech dtmf', 'timeout' => 3,
+    'numDigits' => 1]);
+$gather->say('Please press 1 or say sales for sales.');
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<Response><Say>You entered " . $_REQUEST['Digits'] . "</Say></Response>";
+echo $response;
 ?>
