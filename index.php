be4816c9f5cc77e2fdc21f0966bf3d96d7ac7557
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-13 14:28:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,29 @@ $response = new Twiml();
 if (array_key_exists('Digits', $_POST)) {
     switch ($_POST['Digits']) {
         case 1:
-           $response->say('You selected 1. Good for you!');
-            $gather = $response->gather(['input' => 'speech', 'action' => '/hello']);
-            $gather->say('Welcome to Twilio, please tell us why you\'re calling');
+            $response->say('You selected 1. Good for you!');
+            //$gather = $response->gather(['input' => 'speech', 'action' => '/completed.php', 'hints' => 'cat, numbers, help',]);
+            //$gather->say('Welcome to Twilio, please tell us why you\'re calling');
+            echo"exports.handler = function(context, event, callback) {
+  const twiml = new Twilio.twiml.VoiceResponse();
+ 
+  twiml.gather({
+    input: 'speech',
+    timeout: 3,
+    hints: 'cat, numbers, chuck norris',
+    action: '/hello'
+  }).say('Welcome to the Twilio Facts hotline. Please say cat for cat facts, number for trivia about numbers, or chuck norris for a random chunk of chuck norris knowledge.');
+ 
+  callback(null, twiml);
+};";
+
+
+
 
             break;
         case 2:
-          $response->say('You need support. We will help!');
-            $response->play('ac.mp3', ['loop' => 1]);
+            $response->say('You need support. We will help!');
+            $response->play('ac.mp3', ['loop' => 3]);
             $response->say('All our representatives are currently busy with other calls. Please leave a message with your phone number.');
             break;
         default:
