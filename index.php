247f2792296146510a1763ee7eb43ccafd58a1ef
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-13 13:18:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <?php
+
 require_once './vendor/autoload.php';
 
 use Twilio\Twiml;
 
 $response = new Twiml();
+$response->say('Thanks for calling Rearward Reality. Lets Fight Back.', ['voice' => 'alice', 'language' => 'en']);
+$response->pause(['length' => 1]);
+$response->say('In case of emergency Dial 911.', ['voice' => 'alice', 'language' => 'en']);
+
 
 if (array_key_exists('Digits', $_POST)) {
     switch ($_POST['Digits']) {
-    case 1:
-        $response->say('You selected sales. Good for you!');
-        break;
-    case 2:
-        $response->say('You need support. We will help!');
-        break;
-    default:
-        $response->say('Sorry, I don\'t understand that choice.');
+        case 1:
+            $response->say('You pressed 1');
+            break;
+        case 2:
+            $response->say('You need support. We will help!');
+            break;
+        default:
+            $response->say('Sorry, I don\'t understand that choice.');
     }
 } else {
     // If no input was sent, use the <Gather> verb to collect user input
     $gather = $response->gather(array('numDigits' => 1));
     // use the <Say> verb to request input from the user
-    $gather->say('For sales, press 1. For support, press 2.');
+
+    $gather->say('Press 1 if you want to report the abuse. Press 2 to speak to our customer care executive.');
 
     // If the user doesn't enter input, loop
-  
+    $response->redirect('/voice');
 }
 
 // Render the response as XML in reply to the webhook request
