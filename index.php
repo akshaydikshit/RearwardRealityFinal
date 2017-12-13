b2af196718df5c475ec01bb5956f03349fe9b7df
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-13 14:52:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ if (array_key_exists('Digits', $_POST)) {
             $response->say('You need support. We will help!');
             $response->play('ac.mp3', ['loop' => 1]);
             $response->say('All our representatives are currently busy with other calls. Please leave a message with your phone number.');
-            $response->record(['action' => 'completed.php',
-                'method' => 'GET', 'maxLength' => 10, 'finishOnKey' => '#']);
+            $response->record(['maxLength' => 10, 'finishOnKey' => '#']);
             $response->say('I did not receive a recording');
             break;
         default:
