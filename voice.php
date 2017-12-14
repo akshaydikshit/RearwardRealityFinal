f4f38a715ebf4e7cb6e36521a592e1c542e2debe
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-14 18:03:01 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -1,7 +1,13 @@
 <?php
+
 header('Content-Type: text/xml');
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
+require_once './vendor/autoload.php';
+
+use Twilio\Twiml;
+
+$response = new Twiml();
 switch ($_POST['SpeechResult']) {
     case 'cat':
         $response->say('Fetching Cat!');
