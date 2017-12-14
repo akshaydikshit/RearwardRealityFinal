23d2b9d35f66b3a61324bfc6d145b763091f319e
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-14 17:28:02 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/xml');
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
-switch (SpeechResult) {
+switch ($_POST['SpeechResult']) {
     case 'cat':
         $response->say('Fetching Cat!');
         break;
