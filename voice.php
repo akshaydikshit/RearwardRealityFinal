8bae1cd7f9ebe86c19bb8f522e9832d3d9b52c7b
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-15 16:12:18 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -2,8 +2,6 @@
 
 require_once './vendor/autoload.php';
 
-
-
 use Twilio\Twiml;
 
 $response = new Twiml();
@@ -11,12 +9,20 @@ $response->say('hello how are you', ['voice' => 'woman', 'language' => 'en-US'])
 $response->say('hello how are you', ['voice' => 'woman', 'language' => 'en-US']);
 
 
-
+switch ($_POST['SpeechResult']) {
+    case 'hello':
+        $response->say('I am back');
+        break;
+    case 'Hello':
+        $response->say('You need support. We will help!');
+          break;
+    default:
+        $response->say('Sorry, I don\'t understand that choice.');
+}
 
 
 // Render the response as XML in reply to the webhook request
 header('Content-Type: text/xml');
 echo $response;
-
 ?>
 
