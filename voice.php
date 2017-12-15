22ff0b185482b7389a6e2c2e7aa541acb5bbb123
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-15 16:03:11 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -7,11 +7,10 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-$response->say('hello!', ['voice' => 'woman', 'language' => 'en-US']);
+$response->say('hello', ['voice' => 'woman', 'language' => 'en-US']);
 
 echo $response;
-$response->say('Chapeau!', ['voice' => 'woman', 'language' => 'fr']);
-echo $response;
+
 
 ?>
 
