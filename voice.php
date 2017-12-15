f474c1bc2e0fbe7605c3deff38bb1d9e4c917985
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-15 16:07:58 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -12,5 +12,11 @@ $response->say('hello how are you', ['voice' => 'woman', 'language' => 'en-US'])
 
 
 
+
+
+// Render the response as XML in reply to the webhook request
+header('Content-Type: text/xml');
+echo $response;
+
 ?>
 
