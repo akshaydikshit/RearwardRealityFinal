07606a4c9112ec2ab617a504103d7d86c43d6731
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-15 15:07:56 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -8,9 +8,16 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 
 $response = new Twiml();
-switch ($_POST['SpeechResult']) {
+$response->say('Fetching Cat!');
+switch ($_REQUEST['SpeechResult']) {
     case 'cat':
         $response->say('Fetching Cat!');
+        break;
+     case 'CAT':
+        $response->say('Fetching ALL Cat!');
+        break;
+    case 'Cat':
+        $response->say('Fetching one Cat!');
         break;
     case 'number':
         $response->say('Fetching Number!');
