f3b7b724fa12100c4b8984258b0847f2ad0e2993
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-17 14:02:52 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -6,27 +6,22 @@ use Twilio\Twiml;
 
 $response = new Twiml();
 
-$response->say('hello how are you', ['voice' => 'woman', 'language' => 'en-US']);
-$response->say('hello how are you', ['voice' => 'woman', 'language' => 'en-US']);
 $response->say($_POST['SpeechResult']);
-if (preg_match('/jersey/i', $_POST['SpeechResult']))
-    $response->say('I am in jersey city ');
+if (preg_match('/jersey/i', $_POST['SpeechResult'])) {
+    $response->say('twenty four willow ave ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Jersey city ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('New Jersey zero seven three zero six ', ['voice' => 'woman', 'language' => 'en-US']);
 
-
-switch ($_POST['SpeechResult']) {
-    case "hello":
-        $response->say('I am back');
-        break;
-    case "Hello":
-        $response->say('You need support. We will help!');
-        break;
-    case "HELLO":
-        $response->say('You need support. We will help!');
-        break;
-    default:
-        $response->say('Thats it i am done.');
+    $from = $_REQUEST['From'];
+    $response->sms('The king stay the king.', ['from' => '+16283003802',
+        'to' => $from]);
+    
+    
+    
+} elseif (preg_match('/newyork/i', $_POST['SpeechResult'])) {
+    $response->say('one pace plaza', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('newyork city', ['voice' => 'woman', 'language' => 'en-US']);
 }
-
 
 // Render the response as XML in reply to the webhook request
 header('Content-Type: text/xml');
