6016920211bdf84ae516593b4d5db83cb1d20f60
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-17 14:12:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,10 @@ $response = new Twiml();
 if (array_key_exists('Digits', $_POST)) {
     switch ($_POST['Digits']) {
         case 1:
-            $response->say('You selected 1. Speak your Address!');
-            $response->gather(['input' => 'speech','hints' => 'jersey', 'speechTimeout' => 3,'action' => 'voice.php']);
-            $response->say('I am back');
+            $response->say('You selected 1.', ['voice' => 'woman', 'language' => 'en']);
+            $response->say('Speak your City!', ['voice' => 'woman', 'language' => 'en']);
+
+            $response->gather(['input' => 'speech', 'hints' => 'jersey', 'speechTimeout' => 'auto', 'action' => 'voice.php']);
             break;
         case 2:
             $response->say('You need support. We will help!');
@@ -26,7 +27,7 @@ if (array_key_exists('Digits', $_POST)) {
     // If no input was sent, use the <Gather> verb to collect user input
     $gather = $response->gather(array('numDigits' => 1));
     // use the <Say> verb to request input from the user
-    $gather->say('Press 1 if you want to report the abuse. Press 2 to speak to our customer care executive.');
+    $gather->say('Press 1 if you want to report the abuse. Press 2 to speak to our customer care executive.', ['voice' => 'woman', 'language' => 'en']);
     // If the user doesn't enter input, loop
 }
 // Render the response as XML in reply to the webhook request
