dd7af581cbbb2152be4b26f81db12c7ad31ef55c
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-17 14:14:51 -0500

    Update completed.php

--- completed.php
+++ completed.php
@@ -5,7 +5,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 <Response>
     <Say>
-        Thanks for recording. Goodbye.
+        Thank you for recording a message.We will get back to you as soon as possible. Stay safe Goodbye.
     </Say>
-     <Hangup/>
+    <Hangup/>
 </Response>
