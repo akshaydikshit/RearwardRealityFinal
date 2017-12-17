3b0e5dd8d013115e6b6c7dcca9f2f6db3434ba7c
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-17 14:34:33 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -6,7 +6,7 @@ use Twilio\Twiml;
 
 $response = new Twiml();
 
-$response->say($_POST['SpeechResult']);
+$response->say($_POST['SpeechResult'], ['voice' => 'woman', 'language' => 'en-US']);
 if (preg_match('/jersey/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in jersey city is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('twenty four willow ave ', ['voice' => 'woman', 'language' => 'en-US']);
@@ -16,15 +16,17 @@ if (preg_match('/jersey/i', $_POST['SpeechResult'])) {
     $from = $_REQUEST['From'];
     $response->sms('Address is: 24 Willow Ave, Jersey City,NJ 07306 .', ['from' => '+16283003802',
         'to' => $from]);
+    $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
 } elseif (preg_match('/newyork/i', $_POST['SpeechResult'])) {
-    $response->say('one pace plaza', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('newyork city', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('one pace plaza ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('Newyork city ', ['voice' => 'woman', 'language' => 'en-US']);
     $from = $_REQUEST['From'];
     $response->sms('Address is: 1 Pace Plaza, Newyork City.', ['from' => '+16283003802',
         'to' => $from]);
+    $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
+} else {
+    $response->say('Did you said ', ['voice' => 'woman', 'language' => 'en-US']);
 }
 
 // Render the response as XML in reply to the webhook request
