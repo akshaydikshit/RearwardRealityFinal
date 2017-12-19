1afebf76541c92811b4429f1306aca2d43112070
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-19 17:15:36 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -9,31 +9,41 @@ $response = new Twiml();
 $response->say($_POST['SpeechResult'], ['voice' => 'woman', 'language' => 'en-US']);
 if (preg_match('/mumbai/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in jersey city is at ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('twenty four willow ave ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('Jersey city ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('New Jersey zero seven three zero six ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('One Marine drive ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Mumbai ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Maharashtra 41109 ', ['voice' => 'woman', 'language' => 'en-US']);
 
     $from = $_REQUEST['From'];
-    $response->sms('Address is: 24 Willow Ave, Jersey City,NJ 07306 .', ['from' => '+16283003802',
+    $response->sms('Address is: 1 Marine Drive, Mumbai,Maharashtra 41109 .', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
 } elseif (preg_match('/pune/i', $_POST['SpeechResult'])) {
-    $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('one pace plaza ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('Newyork city ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('The shelter home located in pune is at ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Aroma tower TWO Zero Three ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('M G Road Pune 41110 ', ['voice' => 'woman', 'language' => 'en-US']);
     $from = $_REQUEST['From'];
-    $response->sms('Address is: 1 Pace Plaza, Newyork City.', ['from' => '+16283003802',
+    $response->sms('Address is: 203 Aroma tower  M.G Road Pune.', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
 } elseif (preg_match('/indore/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('one pace plaza ', ['voice' => 'woman', 'language' => 'en-US']);
-    $response->say('Newyork city ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Fourteen Kings Villa ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Indore ', ['voice' => 'woman', 'language' => 'en-US']);
     $from = $_REQUEST['From'];
-    $response->sms('Address is: 1 Pace Plaza, Newyork City.', ['from' => '+16283003802',
+    $response->sms('Address is: 14 Kings Villa, Indore. 41111', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
-} else {
+}
+elseif (preg_match('/delhi/i', $_POST['SpeechResult'])) {
+    $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('M block Queen Road ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Delhi ', ['voice' => 'woman', 'language' => 'en-US']);
+    $from = $_REQUEST['From'];
+    $response->sms('Address is: M-block Queen Road Delhi.41112', ['from' => '+16283003802',
+        'to' => $from]);
+    $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
+} 
+else {
     $response->say('Sorry I did not understand. Please Repeat. ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->gather(['input' => 'speech', 'hints' => 'jersey, newyork', 'speechTimeout' => 'auto', 'action' => 'voice.php']);
 }
