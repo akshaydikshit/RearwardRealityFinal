96d0426aec3c1d6b8268a012066f04310dd8ba95
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-19 17:05:15 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -7,7 +7,7 @@ use Twilio\Twiml;
 $response = new Twiml();
 
 $response->say($_POST['SpeechResult'], ['voice' => 'woman', 'language' => 'en-US']);
-if (preg_match('/jersey/i', $_POST['SpeechResult'])) {
+if (preg_match('/mumbai/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in jersey city is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('twenty four willow ave ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('Jersey city ', ['voice' => 'woman', 'language' => 'en-US']);
@@ -17,7 +17,15 @@ if (preg_match('/jersey/i', $_POST['SpeechResult'])) {
     $response->sms('Address is: 24 Willow Ave, Jersey City,NJ 07306 .', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
-} elseif (preg_match('/newyork/i', $_POST['SpeechResult'])) {
+} elseif (preg_match('/pune/i', $_POST['SpeechResult'])) {
+    $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('one pace plaza ', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('Newyork city ', ['voice' => 'woman', 'language' => 'en-US']);
+    $from = $_REQUEST['From'];
+    $response->sms('Address is: 1 Pace Plaza, Newyork City.', ['from' => '+16283003802',
+        'to' => $from]);
+    $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
+} elseif (preg_match('/indore/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in newyork city is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('one pace plaza ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('Newyork city ', ['voice' => 'woman', 'language' => 'en-US']);
