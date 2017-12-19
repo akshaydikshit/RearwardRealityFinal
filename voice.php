e36adb0bfbab11cb484a60101395b1a68f3eb2cd
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-19 17:56:56 -0500

    Update voice.php

--- voice.php
+++ voice.php
@@ -17,6 +17,10 @@ if (preg_match('/mumbai/i', $_POST['SpeechResult'])) {
     $response->sms('Address is: 1 Marine Drive, Mumbai,Maharashtra 41109 .', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->say('If nothing else Hangup.', ['voice' => 'woman', 'language' => 'en-US']);
+    $response->redirect('control.php', ['method' => 'POST']);
+    
+    
 } elseif (preg_match('/pune/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in pune is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('Aroma tower TWO Zero Three ', ['voice' => 'woman', 'language' => 'en-US']);
@@ -33,8 +37,7 @@ if (preg_match('/mumbai/i', $_POST['SpeechResult'])) {
     $response->sms('Address is: 14 Kings Villa, Indore. 41111', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
-}
-elseif (preg_match('/delhi/i', $_POST['SpeechResult'])) {
+} elseif (preg_match('/delhi/i', $_POST['SpeechResult'])) {
     $response->say('The shelter home located in Delhi is at ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('M block Queen Road ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->say('Delhi ', ['voice' => 'woman', 'language' => 'en-US']);
@@ -42,8 +45,7 @@ elseif (preg_match('/delhi/i', $_POST['SpeechResult'])) {
     $response->sms('Address is: M-block Queen Road Delhi.41112', ['from' => '+16283003802',
         'to' => $from]);
     $response->say('Thank you for calling us. You will also receive a message after this call with the same information.Stay Safe!.', ['voice' => 'woman', 'language' => 'en-US']);
-} 
-else {
+} else {
     $response->say('Sorry I did not understand. Please Repeat. ', ['voice' => 'woman', 'language' => 'en-US']);
     $response->gather(['input' => 'speech', 'hints' => 'mumbai, pune,indore,hyderabad,delhi,gujrat,agra', 'speechTimeout' => 'auto', 'action' => 'voice.php']);
 }
