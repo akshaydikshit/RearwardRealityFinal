de828f1f2fc013a4eef49f077964e52f76b3481a
Akshay <31832289+akshaydikshit@users.noreply.github.com>
2017-12-19 18:04:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,8 @@ require_once './vendor/autoload.php';
 use Twilio\Twiml;
 $response = new Twiml();
 $response->say('Thanks for calling Rearward Reality. Lets Fight Back.', ['voice' => 'woman', 'language' => 'en']);
-$response->say('In case of emergency Dial one zero zero.', ['voice' => 'woman', 'language' => 'en']);
+$response->say('In case of emergency Dial one zero zero.', ['voice' => 'woman', 'language' => 'en','action' => 'control.php']);
 
-// If no input was sent, use the <Gather> verb to collect user input
-$gather = $response->gather(array('numDigits' => 1));
-// use the <Say> verb to request input from the user
-$gather->say('Press 1 to know about the nearest shelter. Press 2 to speak to our representatives.', ['voice' => 'woman', 'language' => 'en', 'action' => 'control.php']);
 
 
 
